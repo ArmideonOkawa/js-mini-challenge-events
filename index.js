2186764846ffd7448699dfe306280c1723b7daef
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-13 17:40:43 -0400

    submit

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ function renderPlayer(player) {
 PLAYERS.forEach(renderPlayer)
 
 /***** End of Starter Code ****/
+/***** Start of my Code ****/
 
-
-
-
+//Elements
+const pList = document.querySelector(".player-container")
+const newPlayer = document.querySelector("#new-player-form")
 /***** Deliverable 1 *****/
 function toggleColor(element) {
   if (element.style.color === "red") {
@@ -38,9 +39,51 @@ function toggleColor(element) {
   } else {
     element.style.color = "red"
   }
-}
+ }
 
+ header.addEventListener('click', e => {
+  toggleColor(document.querySelector("h1#header"))
+ })
 
 /***** Deliverable 2 *****/
 
 /***** Deliverable 3 *****/
+
+// function parseString(string){
+//   const str  = document.querySelector(".likes").innerHTML
+//   const strSplit = str.split(" ");
+//   const pstr = parseInt(strSplit);
+//   const num = pstr + 1;
+//   return num;
+// }
+pList.addEventListener('click', e => {
+  // debugger
+  if (e.target.className === "like-button"){
+    let likeCount = parseInt(event.target.previousElementSibling.textContent) + 1
+    event.target.previousElementSibling.textContent = `${likeCount} likes`
+  }
+})
+
+// const like = document.querySelector(".like-button")
+
+// like.addEventListener('click', event => {
+// console.log("button-clicked")
+// alert("this event works")
+  //const strSplit = string.split(" ");
+
+  //const newLocal = parseInt(strSplit, [0] + 1);
+
+// })
+/*****CONSOLE TESTS*****/
+// string.split(" ")
+// (2) ["1000", "likes"]
+// let strArr = string.split(" ")
+
+// strArr
+// (2) ["1000", "likes"]
+// strArr[0]
+// "1000"
+// parseInt(strArr, [0])
+// 1000
+// parseInt(strArr, [0]) + 1
+// 1001
